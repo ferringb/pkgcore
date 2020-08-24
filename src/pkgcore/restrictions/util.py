8fdfde6cccb6e0a3afe44edb3764cee7cd83bdba
Brian Harring <ferringb@gmail.com>
2020-08-23 20:18:57 -0700

    Extract a generalized visitor for Restriction walking.

--- src/pkgcore/restrictions/util.py
+++ src/pkgcore/restrictions/util.py
@@ -2,18 +2,18 @@
 restriction related utilities
 """
 
+from functools import partial
+
 from snakeoil.sequences import iflatten_func
 
 from pkgcore.restrictions import packages, boolean, restriction
 
+def _restriction_type_filter(desired_type, inst):
+    return getattr(inst, "type", None) == desired_type \
+        and not isinstance(inst, boolean.base)
 
-def _is_package_instance(inst):
-    return (getattr(inst, "type", None) == restriction.package_type and not
-            isinstance(inst, boolean.base))
-
-
-def collect_package_restrictions(restrict, attrs=None, invert=False):
-    """Collect PackageRestriction instances inside a restriction.
+def visit_restrictions(desired_type, restrict, attrs=None, invert=False):
+    """Visit and descend through a restrict for the given restriction type.
 
     :param restrict: package instance to scan
     :param attrs: None (return all package restrictions), or a sequence of
@@ -27,11 +27,14 @@ def collect_package_restrictions(restrict, attrs=None, invert=False):
                 'restrict must be of a restriction.base, '
                 f'not {r.__class__.__class__}: {r!r}'
             )
+
+    i = iflatten_func(restrict, partial(_restriction_type_filter, desired_type))
     if attrs is None:
-        for r in iflatten_func(restrict, _is_package_instance):
-            yield r
-    else:
-        attrs = frozenset(attrs)
-        for r in iflatten_func(restrict, _is_package_instance):
-            if invert == attrs.isdisjoint(getattr(r, 'attrs', ())):
-                yield r
+        return i
+    attrs = frozenset(attrs)
+    return (
+        r for r in i
+        if invert == attrs.isdisjoint(getattr(r, 'attrs', ()))
+    )
+
+collect_package_restrictions = partial(visit_restrictions, restriction.package_type)
